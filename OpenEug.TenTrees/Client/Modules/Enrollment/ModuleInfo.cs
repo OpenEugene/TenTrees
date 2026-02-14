448ec07a0e3ea1d831fdb8e12426b114ddd65cb5
Mark Davis <github@nohorse.com>
2026-02-14 15:03:39 -0800

    Add Tree Mentor column to enrollment list and bump version to 1.2.0
    - Added Tree Mentor column to Index.razor enrollment list view
    
    - Updated English and Xitsonga localization resources
    
    - Incremented module version to 1.2.0 for Issue #19 deployment
    
    Related to #19

--- OpenEug.TenTrees/Client/Modules/Enrollment/ModuleInfo.cs
+++ OpenEug.TenTrees/Client/Modules/Enrollment/ModuleInfo.cs
@@ -9,9 +9,9 @@ namespace OpenEug.TenTrees.Module.Enrollment
         {
             Name = "Enrollment",
             Description = "Grower Enrollment Submission",
-            Version = "1.1.1",
+            Version = "1.2.1",
             ServerManagerType = "",
-            ReleaseVersions = "1.0.0,1.1.0,1.1.1",
+            ReleaseVersions = "1.0.0,1.1.0,1.1.1,1.2.0,1.2.1",
             Dependencies = ""
         };
     }
