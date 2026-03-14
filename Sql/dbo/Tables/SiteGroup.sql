a037330c194370abccbb68b77f523dd3eb73dfa3
Mark Davis <github@nohorse.com>
2026-03-14 15:59:33 -0700

    Add SiteGroup, SiteTask tables; update Site and User schema
    Added SiteGroup, SiteGroupMember, and SiteTask tables to support site grouping and task management features. Modified the Site table by removing HomePageId and adding a CultureCode column. Updated the User table to include a CultureCode column and created a unique index on Username. Updated the project file to include new table scripts.

---
+++ Sql/dbo/Tables/SiteGroup.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[SiteGroup] (
+    [SiteGroupId]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name]          NVARCHAR (200) NOT NULL,
+    [Type]          NVARCHAR (50)  NOT NULL,
+    [PrimarySiteId] INT            NOT NULL,
+    [Synchronize]   BIT            NOT NULL,
+    [CreatedBy]     NVARCHAR (256) NOT NULL,
+    [CreatedOn]     DATETIME2 (7)  NOT NULL,
+    [ModifiedBy]    NVARCHAR (256) NOT NULL,
+    [ModifiedOn]    DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_SiteGroup] PRIMARY KEY CLUSTERED ([SiteGroupId] ASC)
+);
+
---
+++ Sql/dbo/Tables/SiteGroupMember.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[SiteGroupMember] (
+    [SiteGroupMemberId] INT            IDENTITY (1, 1) NOT NULL,
+    [SiteGroupId]       INT            NOT NULL,
+    [SiteId]            INT            NOT NULL,
+    [SynchronizedOn]    DATETIME2 (7)  NULL,
+    [CreatedBy]         NVARCHAR (256) NOT NULL,
+    [CreatedOn]         DATETIME2 (7)  NOT NULL,
+    [ModifiedBy]        NVARCHAR (256) NOT NULL,
+    [ModifiedOn]        DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_SiteGroupMember] PRIMARY KEY CLUSTERED ([SiteGroupMemberId] ASC),
+    CONSTRAINT [FK_SiteGroupMember_Site] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Site] ([SiteId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_SiteGroupMember_SiteGroup] FOREIGN KEY ([SiteGroupId]) REFERENCES [dbo].[SiteGroup] ([SiteGroupId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_SiteGroupMember]
+    ON [dbo].[SiteGroupMember]([SiteId] ASC, [SiteGroupId] ASC) WHERE ([SiteId] IS NOT NULL AND [SiteGroupId] IS NOT NULL);
+
