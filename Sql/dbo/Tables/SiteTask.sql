221e803c5beb32530eb049890d08a1de19937ed3
Mark Davis <github@nohorse.com>
2026-03-14 15:59:33 -0700

    Add SiteGroup, SiteTask tables; update Site and User schema
    Added SiteGroup, SiteGroupMember, and SiteTask tables to support site grouping and task management features. Modified the Site table by removing HomePageId and adding a CultureCode column. Updated the User table to include a CultureCode column and created a unique index on Username. Updated the project file to include new table scripts.

---
+++ Sql/dbo/Tables/SiteTask.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[SiteTask] (
+    [SiteTaskId]  INT            IDENTITY (1, 1) NOT NULL,
+    [SiteId]      INT            NOT NULL,
+    [Name]        NVARCHAR (200) NOT NULL,
+    [Type]        NVARCHAR (200) NOT NULL,
+    [Parameters]  NVARCHAR (MAX) NULL,
+    [IsCompleted] BIT            NULL,
+    [Status]      NVARCHAR (MAX) NULL,
+    [CreatedBy]   NVARCHAR (256) NOT NULL,
+    [CreatedOn]   DATETIME2 (7)  NOT NULL,
+    [ModifiedBy]  NVARCHAR (256) NOT NULL,
+    [ModifiedOn]  DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_SiteTask] PRIMARY KEY CLUSTERED ([SiteTaskId] ASC),
+    CONSTRAINT [FK_SiteTask_Site] FOREIGN KEY ([SiteId]) REFERENCES [dbo].[Site] ([SiteId]) ON DELETE CASCADE
+);
+
