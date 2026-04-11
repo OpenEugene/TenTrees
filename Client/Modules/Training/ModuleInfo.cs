835682d093e9e97f673145679aa82d757629a276
Mark Davis <github@nohorse.com>
2026-04-11 16:25:21 -0700

    Update modules to v2.1.0 and reformat DTO/ViewModel files
    All module versions bumped to 2.1.0 and ReleaseVersions updated.
    DTO and ViewModel files were reformatted or restored with no
    structural changes to their contents.

--- Client/Modules/Training/ModuleInfo.cs
+++ Client/Modules/Training/ModuleInfo.cs
@@ -9,9 +9,9 @@ namespace OpenEug.TenTrees.Module.Training
         {
             Name = "Training",
             Description = "Permaculture training class management and attendance tracking for tree eligibility",
-            Version = "1.0.5",
+            Version = "2.1.0",
             ServerManagerType = "",
-            ReleaseVersions = "1.0.0,1.0.1,1.0.2,1.0.3,1.0.4,1.0.5",
+            ReleaseVersions = "1.0.0,1.0.1,1.0.2,1.0.3,1.0.4,1.0.5,2.1.0",
             Dependencies = "OpenEug.TenTrees.Module.Village.1.0.0,OpenEug.TenTrees.Module.Grower.1.0.0",
             PackageName = "OpenEug.TenTrees.Module.Training"
         };
