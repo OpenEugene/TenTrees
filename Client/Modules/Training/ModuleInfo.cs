1c40ed26cbb357e85ca3d495b2ff9335a8a7aa06
Mark Davis <github@nohorse.com>
2026-07-03 17:27:34 -0700

    Update module versions to 2.5.0 and release history
    Bump version to 2.5.0 for Assessment, Cohort, Enrollment, Mentor, Training, and TreeType modules. Update ReleaseVersions to include 2.4.0 and 2.5.0. No other changes made.

--- Client/Modules/Training/ModuleInfo.cs
+++ Client/Modules/Training/ModuleInfo.cs
@@ -9,9 +9,9 @@ namespace OpenEug.TenTrees.Module.Training
         {
             Name = "Training",
             Description = "Permaculture training class management and attendance tracking for tree eligibility",
-            Version = "2.3.0",
+            Version = "2.5.0",
             ServerManagerType = "",
-            ReleaseVersions = "1.0.0,1.0.1,1.0.2,1.0.3,1.0.4,1.0.5,2.1.0,2.2.0,2.3.0",
+            ReleaseVersions = "1.0.0,1.0.1,1.0.2,1.0.3,1.0.4,1.0.5,2.1.0,2.2.0,2.3.0,2.4.0,2.5.0",
             Dependencies = "OpenEug.TenTrees.Module.Village.1.0.0,OpenEug.TenTrees.Module.Grower.1.0.0",
             PackageName = "OpenEug.TenTrees.Module.Training"
         };
